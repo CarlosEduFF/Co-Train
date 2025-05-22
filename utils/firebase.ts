a5529068e3d92ec6960e7cb6e295964383263c2f
CarlosEduFF <carlos.farias11@etec.sp.gov.br>
2025-05-22 13:48:22 -0300

    Firebase config

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -7,20 +7,15 @@ import { initializeApp } from 'firebase/app';
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
-// Initialize Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDjGBX2IKO9bTBP4NgvXhkLXp_coQzDCp4",
+  authDomain: "co-train-84be9.firebaseapp.com",
+  projectId: "co-train-84be9",
+  storageBucket: "co-train-84be9.firebasestorage.app",
+  messagingSenderId: "620962951335",
+  appId: "1:620962951335:web:d2c72d9b6e2e451058eb82"
 };
 
-const firebase = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
-
-export default firebase;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
